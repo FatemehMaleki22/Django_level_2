9b40da9a7bf80550f5d602049a672c17dc47ab0b
Fatemeh Maleki <fmaleki@itemis.com>
2025-04-25 17:34:17 +0200

    feat:show model in Django template

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from first_app.models import AccessRecord, Topic,Webpage
 
 # Create your views here.
 def index(request):
-  props={'name':'Fatemeh'}
-  return render(request,'first_app/index.html', context=props)
+  webpages_list = AccessRecord.objects.order_by('date')
+  date_dict = {'access_records': webpages_list}
+  return render(request,'first_app/index.html', context=date_dict)
